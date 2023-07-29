389ee9057cb538f73c8cfac38c0ecd53b2a1483b
KL <lamkayin1037@gmail.com>
2023-07-29 19:51:01 -0400

    .

--- a/ContentView.swift
+++ a/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                Image("1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.accentColor)
+                    .overlay(
+                        Button("Welcome") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                            .font(.title)
+                            .tint(Color.gray)
+                            .buttonStyle(.borderedProminent)
+                            .padding(.top,500)
+                    
+                    )
+                    .overlay(
+                        Text("NUTRITIOUS")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                            .padding(.bottom,650)
+                            .padding(.trailing,150)
+                    )
+                    .overlay(
+                    Text("Skin Care")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.leading)
+                        .padding(.bottom,580)
+                        .padding(.trailing,230)
+                    )
+                    .overlay(
+                    Text("Help your skin flourish")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.leading)
+                        .padding(.bottom,520)
+                    )
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
