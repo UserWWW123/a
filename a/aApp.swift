37daced2893f743c6d77e61fb8687a9745c2c8be
KL <lamkayin1037@gmail.com>
2023-07-29 16:17:51 -0400

    Initial Commit

---
+++ a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by KWOK LAM on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
